cc0105987eb93ee643d49f53c1eeb108a14e14bb
larismanisid <laris.manis.endolita@gmail.com>
2026-05-27 20:48:45 +0700

    add some stuff

---
+++ js/horizontalScroll.js
@@ -0,0 +1,22 @@
+function setupHorizontalScroll(wrapperId, leftBtnId, rightBtnId) {
+    const wrapper = document.getElementById(wrapperId);
+
+    if (!wrapper) return;
+
+    const leftBtn = document.getElementById(leftBtnId);
+    const rightBtn = document.getElementById(rightBtnId);
+    
+    if(leftBtn) {
+        leftBtn.addEventListener("click", () => {
+            wrapper.scrollBy({ left: -300, behavior: "smooth" });
+        });
+    }
+    if(rightBtn) {
+        rightBtn.addEventListener("click", () => {
+            wrapper.scrollBy({ left: 300, behavior: "smooth" });
+        });
+    }
+}
+
+setupHorizontalScroll("cateringWrapper", "scrollCateringLeftBtn", "scrollCateringRightBtn");
+setupHorizontalScroll("snackWrapper", "scrollSnackLeftBtn", "scrollSnackRightBtn");
